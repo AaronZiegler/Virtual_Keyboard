94aaac99f932d071cbb3bf3fee3eccf2a29f6f23
AaronZiegler <zieglaar21@7hills.org>
2021-03-10 09:40:19 -0500

    Started to implement a key press event to find out when keys on the keyboard are pressed

--- Virtual_Keyboard.git/Keyboard.java
+++ Virtual_Keyboard.git/Keyboard.java
@@ -4,14 +4,34 @@
 * Key guide: A-K, C-B
 * Have effects
 * maybe some recording
+* Have some UI
 */
 
-import doodlepad.jar;
-
+import doodlepad.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 
 public class Keyboard{
+	
+	
+	
 	public static void main(String[] args){
-		//test test test
+		MKeyListener keyPress = new MKeyListener();
+	}
+}
+
+class MKeyListener extends KeyAdapter {
+	@Override
+    public void keyPressed(KeyEvent event) {
+		char ch = event.getKeyChar();
+		
+	  	if (ch == 'a' ||ch == 'b'||ch == 'c' ) {
+		  	System.out.println(event.getKeyChar());
+	  	}
+		
+	  	if (event.getKeyCode() == KeyEvent.VK_HOME) {
+			System.out.println("Key codes: " + event.getKeyCode());
+		}
 	}
 }

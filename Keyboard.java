060a66e12342113683239b9c7d1f346f856b03a9
AaronZiegler <zieglaar21@7hills.org>
2021-03-10 09:02:06 -0500

    First Commit af asdf asd fas df
    asdf

--- Keyboard.java
+++ Keyboard.java
@@ -1,5 +1,15 @@
+/* Aaron Ziegler
+* Keyboard
+* A local keyboard program to play
+* Key guide: A-K, C-B
+* Have effects
+* maybe some recording
+*/
+
 import doodlepad.jar;
 
+
+
 public class Keyboard{
-	
+
 }
